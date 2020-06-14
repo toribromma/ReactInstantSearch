a3fcfb11caf1d6105ffbc72ad4b1f82737b569bc
toritheterrible <torimpham@gmail.com>
2020-06-14 11:03:06 -0700

    added instant search with credentials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import { InstantSearch } from 'react-instantsearch-dom';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  
+      <InstantSearch
+      appId="latency"
+      apiKey="3d9875e51fbd20c7754e65422f7ce5e1"
+      indexName="bestbuy"
+      ></InstantSearch>
     </div>
   );
 }
